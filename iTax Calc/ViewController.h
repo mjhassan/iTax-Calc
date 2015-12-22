d65bd2de977859023710f44775a9bdb347ca883c
Jahid Hassan <jahid.hassan.ce@gmail.com>
2015-12-22 08:23:37 +0600

    Initial Commit

---
+++ iTax Calc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iTax Calc
+//
+//  Created by Jahid on 12/22/15.
+//  Copyright © 2015 Jahid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iTax Calc/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iTax Calc
+//
+//  Created by Jahid on 12/22/15.
+//  Copyright © 2015 Jahid. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
